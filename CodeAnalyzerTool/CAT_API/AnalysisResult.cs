add032231cbefb9eee8fca4e6f07c82e43b6dd7c
MichelMattheIS <michel.matthe@infosupport.com>
2023-04-27 17:07:39 +0200

    started making Plugin API

---
+++ CodeAnalyzerTool/CAT_API/AnalysisResult.cs
@@ -0,0 +1,10 @@
+﻿namespace CAT_API;
+
+
+public class AnalysisResult {
+    public string RuleId { get; set; }
+    public Location location { get; set; }
+    public string PluginId { get; set; }
+    // What (programming) language the analyzer is analysing (i.e. c#)
+    public string AnalysisTarget { get; set; }
+}
