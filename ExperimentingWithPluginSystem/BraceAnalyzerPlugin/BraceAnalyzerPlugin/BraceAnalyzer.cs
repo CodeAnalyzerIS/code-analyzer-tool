dd4cd7e97641571ca7d1bad61e55ef071592bdb5
alexanderwuytsIS <alexander.wuyts@infosupport.com>
2023-04-28 13:22:06 +0200

    Cleanup BraceAnalyzerPlugin Project

---
+++ ExperimentingWithPluginSystem/BraceAnalyzerPlugin/BraceAnalyzerPlugin/BraceAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace BraceAnalyzerPlugin;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class BraceAnalyzer : DiagnosticAnalyzer
+{
+    public const string DiagnosticId = "BraceAnalyzer";
+
+    // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+    // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+    private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.BraceAnalyzerTitle),
+        Resources.ResourceManager, typeof(Resources));
+
+    private static readonly LocalizableString MessageFormat =
+        new LocalizableResourceString(nameof(Resources.BraceAnalyzerMessageFormat), Resources.ResourceManager,
+            typeof(Resources));
+
+    private static readonly LocalizableString Description =
+        new LocalizableResourceString(nameof(Resources.BraceAnalyzerDescription), Resources.ResourceManager,
+            typeof(Resources));
+
+    private const string Category = "Naming";
+
+    private static readonly DiagnosticDescriptor Rule = new (DiagnosticId, Title, MessageFormat,
+        Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxTreeAction(AnalyzeBlockStatements);
+    }
+
+    private static void AnalyzeBlockStatements(SyntaxTreeAnalysisContext ctx)
+    {
+        var root = ctx.Tree.GetRoot(ctx.CancellationToken);
+        foreach (var statement in root.DescendantNodes().OfType<StatementSyntax>())
+        {
+            if (statement is BlockSyntax) continue;
+
+            if (statement.Parent is StatementSyntax && !(statement.Parent is BlockSyntax))
+            {
+                var diagnostic = Diagnostic.Create(Rule, statement.GetFirstToken().GetLocation());
+                ctx.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
--- ExperimentingWithPluginSystem/BraceAnalyzerPlugin/BraceAnalyzerPlugin/Resources.Designer.cs
+++ ExperimentingWithPluginSystem/BraceAnalyzerPlugin/BraceAnalyzerPlugin/Resources.Designer.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PluginTest2 {
+namespace BraceAnalyzerPlugin {
     using System;
     
     
@@ -38,7 +38,7 @@ namespace PluginTest2 {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PluginTest2.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BraceAnalyzerPlugin.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -60,7 +60,7 @@ namespace PluginTest2 {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Enclose statement with curly braces.
+        ///   Looks up a localized string similar to Enclose statement with curly braces..
         /// </summary>
         internal static string BraceAnalyzerDescription {
             get {
