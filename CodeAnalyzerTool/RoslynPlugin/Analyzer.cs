e9bb6732efe52d94dcbcb155bd74793e7b7d9811
alexanderwuytsIS <alexander.wuyts@infosupport.com>
2023-05-03 17:15:25 +0200

    Removed local working directory

--- CodeAnalyzerTool/RoslynPlugin/Analyzer.cs
+++ CodeAnalyzerTool/RoslynPlugin/Analyzer.cs
@@ -14,8 +14,7 @@ public static class Analyzer
     internal static async Task<ImmutableArray<Diagnostic>> StartAnalysis(MSBuildWorkspace workspace,
         PluginConfig pluginConfig, string pluginsPath)
     {
-        // var workingDirectory = Directory.GetCurrentDirectory();
-        var workingDirectory = @"C:\Users\AlexanderW\Documents\Blazor-CRUD-webapp";
+        var workingDirectory = Directory.GetCurrentDirectory();
         var solutionPaths = Directory.GetFiles(workingDirectory, "*.sln", SearchOption.AllDirectories);
         var diagnosticResults = new List<Diagnostic>();
 
