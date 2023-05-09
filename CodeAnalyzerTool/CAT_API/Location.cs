4cf66a361351cb0aeda98a240471322f6158444c
MichelMattheIS <michel.matthe@infosupport.com>
2023-05-09 14:00:12 +0200

    removed default constructors from CAT_API model classes

--- CodeAnalyzerTool/CAT_API/Location.cs
+++ CodeAnalyzerTool/CAT_API/Location.cs
@@ -2,15 +2,10 @@
 
 public class Location {
     public int Id { get; set; }
-    // Normally only possible to create AnalysisResult instance with public constructor so Path, FileExtension cannot be null
-    public string Path { get; set; } = null!;
+    public string Path { get; set; }
     public int StartLine { get; set; }
     public int EndLine { get; set; }
-    public string FileExtension { get; set; } = null!;
-    
-    // Only meant for Entity Framework
-    // ReSharper disable once UnusedMember.Local
-    private Location() {}
+    public string FileExtension { get; set; }
 
     public Location(string path, int startLine, int endLine, string fileExtension)
     {
