d795c118191abc5c4fe5d609008de1ed5f3ac504
MichelMattheIS <michel.matthe@infosupport.com>
2023-05-10 12:44:04 +0200

    Refactored Console WriteLine's in RoslynPlugin to Logs

--- CodeAnalyzerTool/RoslynPlugin/ProgressReporter.cs
+++ CodeAnalyzerTool/RoslynPlugin/ProgressReporter.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.CodeAnalysis.MSBuild;
+using Serilog;
 
 namespace RoslynPlugin;
 
 //This class shows loading progress in the console
-public class ConsoleProgressReporter : IProgress<ProjectLoadProgress> {
+public class ProgressReporter : IProgress<ProjectLoadProgress> {
     public void Report(ProjectLoadProgress loadProgress) {
         var projectDisplay = Path.GetFileName(loadProgress.FilePath);
         if (loadProgress.TargetFramework != null) {
             projectDisplay += $" ({loadProgress.TargetFramework})";
         }
-
-        Console.WriteLine(
-            $@"{loadProgress.Operation,-15} {loadProgress.ElapsedTime,-15:m\:ss\.fffffff} {projectDisplay}");
+        var operation = $"{loadProgress.Operation,-15}";
+        var elapsedTime = @$"{loadProgress.ElapsedTime,-15:m\:ss\.fffffff}";
+        Log.Debug("{Operation} {ElapsedTime} {ProjectDisplay}", operation, elapsedTime, projectDisplay);
     }
 }
