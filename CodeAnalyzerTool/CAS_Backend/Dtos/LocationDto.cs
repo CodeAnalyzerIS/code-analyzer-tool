ca5ae63a119f3a58f1d279950edc1a07a409cc73
Alexander Wuyts <alexander.wuyts@infosupport.com>
2023-05-11 16:06:56 +0200

    Started working on backendService

---
+++ CodeAnalyzerTool/CAS_Backend/Dtos/LocationDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CAS_Backend.Dtos;
+
+public class LocationDto
+{
+    public int Id { get; set; }
+    [Required] public string Path { get; set; }
+    [Range(0, int.MaxValue)] public int StartLine { get; set; }
+    [Range(0, int.MaxValue)] public int EndLine { get; set; }
+    [Required] public string FileExtension { get; set; }
+    
+    public LocationDto(string path, int startLine, int endLine, string fileExtension)
+    {
+        Path = path;
+        StartLine = startLine;
+        EndLine = endLine;
+        FileExtension = fileExtension;
+    }
+}
