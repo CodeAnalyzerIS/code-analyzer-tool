f74c28fc2fb18b5f5276285fe012636ecedeef38
Alexander Wuyts <alexander.wuyts@infosupport.com>
2023-05-11 16:06:56 +0200

    Started working on backendService

---
+++ CodeAnalyzerTool/CAS_Backend/CasContext.cs
@@ -0,0 +1,33 @@
+﻿using CAT_API;
+using Microsoft.EntityFrameworkCore;
+
+namespace CAS_Backend;
+
+public class CasContext : DbContext
+{
+    public CasContext(DbContextOptions<CasContext> options) : base(options)
+    {
+    }
+
+    public DbSet<AnalysisResult> AnalysisResults { get; set; } = null!;
+    public DbSet<Location> Locations { get; set; } = null!;
+    public DbSet<Rule> Rules { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        ConfigureAnalysisResults(modelBuilder);
+    }
+
+    private void ConfigureAnalysisResults(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AnalysisResult>()
+            .OwnsOne(ar => ar.Location);
+
+        modelBuilder.Entity<AnalysisResult>()
+            .HasOne(ar => ar.Rule)
+            .WithMany(r => r.AnalysisResults)
+            .HasForeignKey("FK_AnalysisResult_Rule");
+    }
+}
