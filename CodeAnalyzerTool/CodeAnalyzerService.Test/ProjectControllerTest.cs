d0a80009231561ade3202c9d8e75538add5f6d9d
alexanderwuytsIS <alexander.wuyts@infosupport.com>
2023-05-22 09:06:19 +0200

    Fixed putproject bug

--- CodeAnalyzerTool/CodeAnalyzerService.Test/ProjectControllerTest.cs
+++ CodeAnalyzerTool/CodeAnalyzerService.Test/ProjectControllerTest.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using AutoFixture;
 using CodeAnalyzerService.Backend.Controllers;
 using CodeAnalyzerService.Backend.Dtos;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodeAnalyzerService.Test;
 
@@ -17,13 +19,22 @@ public class ProjectControllerTest : IClassFixture<TestDatabaseFixture>
     [Fact]
     public async Task PutProjectReturnsCreatedAt()
     {
+        //Arrange
         var autoFixture = new Fixture();
         // var ruleViolationDto = autoFixture.Build<RuleViolationDto>().With(rv => rv.Rule, "Info").CreateMany(3);
         // var projectAnalysisDto = autoFixture.Build<ProjectAnalysisDto>().With(pa => pa.RuleViolations, new List<RuleViolationDto>(){ruleViolationDto}).Create();
         var projectAnalysisDto = autoFixture.Create<ProjectAnalysisDto>();
-        using var context = Fixture.CreateContext();
+        await using var context = Fixture.CreateContext();
         var controller = new ProjectController(context);
+        
+        //Act
         var ar = await controller.PutProject(projectAnalysisDto);
+
+        //Assert
+        var result = ar.Result as CreatedAtActionResult;
+        result.Should().NotBeNull();
+        Assert.Single(context.Projects);
+        
         await context.Database.EnsureDeletedAsync();
         Debugger.Break();
     }
