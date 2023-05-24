c03414cfbd49bb53e369f48a50a9a97de4c5c07e
alexanderwuytsIS <alexander.wuyts@infosupport.com>
2023-05-24 17:15:56 +0200

    Possibly added version to github schema

--- CodeAnalyzerTool/CodeAnalyzerTool/ConfigTemplateGenerator.cs
+++ CodeAnalyzerTool/CodeAnalyzerTool/ConfigTemplateGenerator.cs
@@ -1,12 +1,15 @@
-﻿namespace CodeAnalyzerTool;
+﻿using System.Reflection;
+
+namespace CodeAnalyzerTool;
 
 public static class ConfigTemplateGenerator
 {
     internal static async Task GenerateConfigTemplate()
     {
+        var version = Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "main";
         var projectName = Directory.GetCurrentDirectory().Split(Path.DirectorySeparatorChar).Last();
         var template = $@"{{
-    ""$schema"": ""https://raw.githubusercontent.com/CodeAnalyzerIS/code-analyzer-tool/main/CATSchema.json"",
+    ""$schema"": ""https://raw.githubusercontent.com/CodeAnalyzerIS/code-analyzer-tool/{version}/CATSchema.json"",
     ""projectName"": ""{projectName}"",
     ""plugins"": []
 }}";
