b371a2d3824295c76b3d9204b0ff1a9535f9de89
alexanderwuytsIS <alexander.wuyts@infosupport.com>
2023-05-26 10:58:06 +0200

    First version of Project Card

--- CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/App.tsx
+++ CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/App.tsx
@@ -3,17 +3,17 @@ import './App.css';
 import {QueryClient, QueryClientProvider} from "react-query";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Home from "./components/Home";
-import {AppBar, Avatar, Button, IconButton, Toolbar, Typography} from "@mui/material";
+import {AppBar, Avatar, Toolbar, Typography} from "@mui/material";
+
+export const BACKEND_URL = 'http://localhost:5082';
 
 const queryClient = new QueryClient()
 
 const Header = () => (
     <AppBar position="static" color="transparent">
-        <Toolbar sx={{justifyContent: "space-between"}}>
-            <div className="headerBar">
-                <Avatar src={'logo_transparent.png'}/>
-                <Typography variant="h6" sx={{justifySelf: "center"}}>Code Analyzer Tool</Typography>
-            </div>
+        <Toolbar>
+            <Avatar src={'logo_transparent.png'}/>
+            <Typography variant="h6" sx={{color: '#15B7B9', justifySelf: "center"}}>Code Analyzer Tool</Typography>
         </Toolbar>
     </AppBar>
 );
--- CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/Home.tsx
+++ CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/Home.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import {useProject} from "../hooks/useProject";
 import Loading from "./Loading";
-import {Alert, Card, CardContent, CardHeader, Stack, Typography} from "@mui/material";
+import {Alert, Box, Card, CardContent, CardHeader, Stack, Typography} from "@mui/material";
+import {useProjectOverview} from "../hooks/useProjectOverview";
+import TroubleshootIcon from '@mui/icons-material/Troubleshoot';
+import {ProjectOverview} from "../model/ProjectOverview";
+import ReportIcon from '@mui/icons-material/Report';
 
 export default function Home() {
-    const {isLoading, isError, project} = useProject(1)
+    const {isLoading, isError, projectOverviews} = useProjectOverview()
 
     if (isLoading) {
         return <Loading/>
@@ -12,17 +15,33 @@ export default function Home() {
     if (isError){
         return <Alert severity="error">Error loading the project</Alert>
     }
-    console.log(project)
+    console.log(projectOverviews)
 
     return(
-        <Card sx={{width: "50%"}}>
-            <CardHeader title={project.projectName}/>
-            <CardContent>
-                <Stack direction="row" spacing={'space-between'}>
-                    <Typography>{project.analyses[project.analyses.length - 1].createdOn.toLocaleString("en-US")}</Typography>
-                    <Typography>{project.analyses[project.analyses.length - 1].ruleViolations.length}</Typography>
-                </Stack>
-            </CardContent>
-        </Card>
+        <Box sx={{display: 'flex', justifyContent: 'center', mt: 5}}>
+            {projectOverviews.map((po:ProjectOverview)  => (
+                <Card sx={{width: "50%", border: 'solid', borderColor: '#c4c4c4', borderWidth: 'thin'}} key={po.id}>
+                    <CardHeader
+                        style={{ textAlign: 'center', color: '#6574FC', borderBottom: 'solid', borderColor: '#c4c4c4',
+                            borderWidth: 'thin' }}
+                        title={po.projectName}
+                    />
+                    <CardContent>
+                        <Box sx={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly',
+                            alignItems: 'center', color: '#15B7B9'}}>
+                            <Stack direction='row' spacing={1}>
+                                <TroubleshootIcon/>
+                                <Typography>Last Analysis: {po.lastAnalysisDate.toString()}</Typography>
+                            </Stack>
+
+                            <Stack direction='row' spacing={1}>
+                                <ReportIcon/>
+                                <Typography>Rule Violations: {po.ruleViolationCount}</Typography>
+                            </Stack>
+                        </Box>
+                    </CardContent>
+                </Card>
+            ))}
+        </Box>
     )
 }
