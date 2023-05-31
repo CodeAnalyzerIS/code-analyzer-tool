e4b2d378880b96f419f3b433fb0512da51e4c238
MichelMattheIS <michel.matthe@infosupport.com>
2023-05-31 14:50:27 +0200

    added tests for UnnecessaryTypeCastRule

---
+++ CodeAnalyzerTool/RoslynPlugin.Test/Rules/UnnecessaryTypeCastTest.cs
@@ -0,0 +1,201 @@
+﻿using RoslynPlugin.rules;
+
+namespace RoslynPlugin.Test.Rules;
+
+public class UnnecessaryTypeCastTest
+{
+
+    private async Task ShouldReport(string code)
+    {
+        var rule = new UnnecessaryTypeCastRule();
+        var results = await RuleTestRunner.CompileStringWithRule(code, rule);
+        results.Should().Contain(rv => rv.Rule.RuleName == RuleNames.UNNECESSARY_TYPE_CAST_RULE);
+    }
+
+    private async Task ShouldNotReport(string code)
+    {
+        var rule = new UnnecessaryTypeCastRule();
+        var results = await RuleTestRunner.CompileStringWithRule(code, rule);
+        results.Should().BeEmpty();
+    }
+
+    public async Task UnnecessaryTypeCastRule_ShouldReportRuleViolation_WhenCastToDerivedType()
+    {
+        var code = @"
+class C
+{
+    void M()
+    {
+        var c = new C();
+
+        var s = ((D)c).I;
+
+    }
+
+    public int I { get; set; }
+}
+
+class D : C
+{
+}
+";
+        await ShouldReport(code);
+    }
+
+    public async Task UnnecessaryTypeCastRule_ShouldReportRuleViolation_WhenCastToDerivedTypeWithConditionalAccess()
+    {
+        var code = @"
+class C
+{
+    void M()
+    {
+        var c = new C();
+
+        var s = ((D)c)?.I;
+
+    }
+
+    public int I { get; set; }
+}
+
+class D : C
+{
+}
+";
+        await ShouldReport(code);
+    }
+
+    public async Task UnnecessaryTypeCastRule_ShouldReportRuleViolation_WhenCastToImplementedInterface()
+    {
+        var code = @"
+using System.Collections.Generic;
+
+class C
+{
+    void M()
+    {
+        List<string> list = new List<string>{""hello world""};
+
+        string s = ((IList<string>)list)[0];
+    }
+}
+";
+        await ShouldReport(code);
+    }
+
+    public async Task UnnecessaryTypeCastRule_ShouldReportRuleViolation_WhenCastToImplementedInterfaceWithConditionalAccess()
+    {
+        var code = @"
+using System.Collections.Generic;
+
+class C
+{
+    void M()
+    {
+        List<string> list = new List<string>{""hello world""};
+
+        string s = ((IList<string>)list)?[0];
+    }
+}
+";
+        await ShouldReport(code);
+    }
+
+
+
+//     public async Task Test_Accessibility()
+//     {
+//         var code = @"
+// class B
+// {
+//     private void M(B b)
+//     {
+//         ((C)b).Protected();
+//
+//         ((C)b).PrivateProtected();
+//
+//         ((C)b).ProtectedInternal();
+//     }
+//
+//     private class C : B
+//     {
+//         private void M2(B b)
+//         {
+//             ((C)b).Protected();
+//
+//             ((C)b).PrivateProtected();
+//
+//             ((C)b).ProtectedInternal();
+//         }
+//     }
+//
+//     protected void Protected() { }
+//
+//     private protected void PrivateProtected() { }
+//
+//     protected internal void ProtectedInternal() { }
+// }
+// ";
+//     }
+//
+//     public async Task Test_Accessibility_ProtectedInternal()
+//     {
+//         var code = @"
+// class C : B
+// {
+//     public static void M()
+//     {
+//         var b = default(B);
+//
+//         ([|(C)|]b).ProtectedInternal();
+//     }
+// }
+//
+// class B
+// {
+//     protected internal void ProtectedInternal() { }
+// }
+// ";
+//     }
+
+    public async Task UnnecessaryTypeCastRule_ShouldNotReportRuleViolation_WhenPrivateProtectedOtherwiseNotAccesible()
+    {
+        var code = @"
+class B
+{
+    private protected void PrivateProtected() { }
+}
+
+class C : B
+{
+    void M(B b)
+    {
+        ((C)b).PrivateProtected();
+    }
+}
+";
+        await ShouldNotReport(code);
+    }
+
+    public async Task UnnecessaryTypeCastRule_ShouldNotReportRuleViolation_WhenCastToDerivedType()
+    {
+        var code = @"
+class C
+{
+    void M()
+    {
+        var c = new C();
+
+        var s = ((B)c).P;
+
+    }
+}
+
+class B : C
+{
+    public string P { get; set; }
+}
+";
+        await ShouldNotReport(code);
+    }
+}
---
+++ CodeAnalyzerTool/RoslynPlugin/Rules/UnnecessaryTypeCastRule.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+using RoslynPlugin.API;
+
+namespace RoslynPlugin.rules;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class UnnecessaryTypeCastRule : RoslynRule
+{
+    public sealed override string RuleName => RuleNames.MAKE_LOCAL_VARIABLE_CONSTANT_RULE;
+    public sealed override DiagnosticSeverity Severity { get; set; }
+    public sealed override Dictionary<string, string> Options { get; set; }
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
+    private const string CATEGORY = RuleCategories.PERFORMANCE;
+    private readonly DiagnosticDescriptor _rule;
+
+    private static readonly LocalizableString Title = new LocalizableResourceString(
+        nameof(Resources.MakeLocalVariableConstantTitle), Resources.ResourceManager, typeof(Resources));
+
+    private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+        nameof(Resources.MakeLocalVariableConstantMessageFormat), Resources.ResourceManager, typeof(Resources));
+
+    private static readonly LocalizableString Description = new LocalizableResourceString(
+        nameof(Resources.MakeLocalVariableConstantDescription), Resources.ResourceManager, typeof(Resources));
+
+    public UnnecessaryTypeCastRule()
+    {
+        Options = new Dictionary<string, string>();
+        Severity = DiagnosticSeverity.Warning;
+        _rule = new DiagnosticDescriptor(RuleName, Title, MessageFormat, CATEGORY, Severity, true, Description);
+        SupportedDiagnostics = ImmutableArray.Create(_rule);
+    }
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSyntaxNodeAction(AnalyzeTypeCastExpression, SyntaxKind.CastExpression);
+    }
+
+    private void AnalyzeTypeCastExpression(SyntaxNodeAnalysisContext context)
+    {
+    }
+}
