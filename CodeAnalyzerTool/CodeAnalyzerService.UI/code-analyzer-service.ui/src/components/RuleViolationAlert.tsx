31d06b6a966421bd8bcfd890d5dba7d38a97416f
MichelMattheIS <michel.matthe@infosupport.com>
2023-06-06 00:31:54 +0200

    fixed key bug on .map in AnalysisSummary.tsx and added links to RuleDetail page

--- CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/RuleViolationAlert.tsx
+++ CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/RuleViolationAlert.tsx
@@ -1,16 +1,20 @@
 import {getMUISeverity} from "../util/HelperFunctions";
-import {Alert, AlertTitle} from "@mui/material";
+import {Alert, AlertTitle, Button, Stack} from "@mui/material";
 import React from "react";
 import {RuleViolation} from "../model/RuleViolation";
+import {useNavigate} from "react-router-dom";
 
 interface RuleViolationAlertProps {
     violation: RuleViolation;
 }
 
 export default function RuleViolationAlert({violation}: RuleViolationAlertProps) {
+    const navigate = useNavigate();
+
     return (
-        <Alert severity={getMUISeverity(violation.severity)} sx={{paddingY: 'unset', borderRadius: 'unset'}}>
-            <AlertTitle>{violation.message}</AlertTitle>
+        <Alert severity={getMUISeverity(violation.severity)} sx={{paddingY: 'unset', borderRadius: 'unset'}}
+               onClick={() => navigate(`/rule/${violation.rule.id}`)}>
+            <AlertTitle sx={{cursor: "pointer"}}>{violation.message}</AlertTitle>
             <small>Line {violation.location.startLine}</small>
         </Alert>
     )
