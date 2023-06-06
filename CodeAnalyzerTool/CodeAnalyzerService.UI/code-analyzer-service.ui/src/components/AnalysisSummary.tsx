2890a570b4eb65a5b98399c5393d9a559a80e86d
MichelMattheIS <michel.matthe@infosupport.com>
2023-06-06 10:46:46 +0200

    Added Empty state placeholder to AnalysisSummary.tsx and made its grid header responsive

--- CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/AnalysisSummary.tsx
+++ CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/AnalysisSummary.tsx
@@ -1,7 +1,6 @@
 import {
-    Alert,
+    Alert, Container, Divider,
     FormControl, Grid, MenuItem, Select, SelectChangeEvent,
-    Stack,
     Typography
 } from "@mui/material";
 import {groupRuleViolationsByPath} from "../util/HelperFunctions";
@@ -10,6 +9,7 @@ import {useAnalysis} from "../hooks/useAnalysis";
 import Loading from "./Loading";
 import {AnalysisHistory} from "../model/Analysis";
 import FileViolations from "./FileViolations";
+import DonePlaceholder from "./placeholders/DonePlaceholder";
 
 interface AnalysisSummaryProps {
     initialAnalysisId: number;
@@ -32,33 +32,45 @@ export default function AnalysisSummary({initialAnalysisId, analysisHistory}: An
         setAnalysisId(newAnalysisId);
     };
 
-    return (
-        <Stack sx={{width: '60%', mt: 3}} spacing={2}>
-            <Grid container alignItems='center'>
-                <Grid item xs={2}>
-                    <FormControl fullWidth>
-                        <Select
-                            labelId="select-analysis-label"
-                            id="select-analysis"
-                            value={analysis.id.toString()}
-                            onChange={handleChange}
-                        >
-                            {analysisHistory.map((analysisItem, index) => (
-                                <MenuItem key={index} value={analysisItem.id}>{analysisItem.createdOn.toLocaleString('nl-BE')}</MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={8}>
-                    <Typography sx={{textAlign: 'center'}}>Analysis Results</Typography>
-                </Grid>
-                <Grid item xs={2}>
-                    <Typography sx={{textAlign: 'end'}}><strong>{analysis.ruleViolations.length}</strong> Rule Violations</Typography>
-                </Grid>
+    function SummaryHead({analysisId, ruleViolationCount}: {analysisId: string, ruleViolationCount: number}) {
+        return <Grid container alignItems='center'>
+            <Grid item xs={12} sm={4} md={3} lg={2}>
+                <FormControl fullWidth>
+                    <Select
+                        labelId="select-analysis-label"
+                        id="select-analysis"
+                        value={analysisId}
+                        onChange={handleChange}
+                    >
+                        {analysisHistory.map((analysisItem, index) => (
+                            <MenuItem key={index} value={analysisItem.id}>
+                                <Typography textAlign="center">
+                                    {analysisItem.createdOn.toLocaleString('nl-BE')}
+                                </Typography>
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
             </Grid>
-            {Object.entries(groupedAnalysis).map(([path, violations], index) => (
-                <FileViolations key={index} path={path} violations={violations}/>
-            ))}
-        </Stack>
+            <Grid item xs={6} sm={4} md={6} lg={8} pt={1}>
+                <Typography sx={{textAlign: 'center'}}>Analysis Results</Typography>
+            </Grid>
+            <Grid item xs={6} sm={4} md={3} lg={2} pt={1}>
+                <Typography sx={{textAlign: 'center'}}><strong>{ruleViolationCount}</strong> Rule
+                    Violations</Typography>
+            </Grid>
+        </Grid>;
+    }
+
+    return (
+        <Container sx={{mt: 3}} maxWidth="lg">
+            <SummaryHead analysisId={analysis.id.toString()} ruleViolationCount={analysis.ruleViolations.length}/>
+            <Divider sx={{mt: 2}}/>
+            {analysis.ruleViolations.length < 1 ? <DonePlaceholder/>
+                :
+                Object.entries(groupedAnalysis).map(([path, violations], index) => (
+                    <FileViolations key={index} path={path} violations={violations}/>
+                ))}
+        </Container>
     )
 }
