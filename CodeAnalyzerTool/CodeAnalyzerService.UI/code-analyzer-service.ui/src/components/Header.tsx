7fc00793d0f913d3c22dc6cc79a85288ba3fe329
alexanderwuytsIS <alexander.wuyts@infosupport.com>
2023-06-07 11:18:49 +0200

    Made header icon a home button

--- CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/Header.tsx
+++ CodeAnalyzerTool/CodeAnalyzerService.UI/code-analyzer-service.ui/src/components/Header.tsx
@@ -1,17 +1,18 @@
 import React, {useContext} from "react";
 import BreadcrumbContext, {IBreadcrumbContext} from "../context/BreadcrumbContext";
 import {AppBar, Avatar, Breadcrumbs, Stack, Toolbar, Typography, useTheme} from "@mui/material";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Header() {
     const {breadcrumbData} = useContext<IBreadcrumbContext>(BreadcrumbContext)
     const palette = useTheme().palette;
+    const navigate = useNavigate()
 
     return(
         <AppBar position="static" color="transparent">
             <Toolbar variant='dense' sx={{justifyContent: 'center'}}>
                 <Stack direction='row' spacing={1} alignItems='center' sx={{position: 'absolute', left: 10}}>
-                    <Avatar src={'/logo_transparent.png'}/>
+                    <Avatar src={'/logo_transparent.png'} onClick={() => navigate('/')} sx={{cursor: 'pointer'}}/>
                     <Breadcrumbs aria-label='breadcrumb'>
                         {breadcrumbData.map((breadcrumb, index) => (
                             <Link key={index} style={{color: palette.secondary.dark}} to={breadcrumb.path}>
